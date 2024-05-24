8e59d95225fd2bda539e4f36fd1875b8639f2de9
solaluset <60041069+solaluset@users.noreply.github.com>
2024-05-24 23:18:36 +0300

    add interactive console

--- yabi/__init__.py
+++ yabi/__init__.py
@@ -1,11 +1,13 @@
 import sys
 from io import TextIOBase
+from codeop import CommandCompiler
+from traceback import print_exc
 
 import pwcp
 from pwcp import main as p_main
 from pwcp.preprocessor import PyPreprocessor, preprocess
 
-from .parser import to_bython, to_pure_python
+from .parser import UNCLOSED_BLOCK_ERROR, to_bython, to_pure_python
 
 
 EXTENSION = ".by"
@@ -25,7 +27,57 @@ def main(args=None):
     pwcp.config.FILE_EXTENSIONS.append(EXTENSION)
     pwcp.preprocessor.preprocess = preproc
     args = (args,) if args is not None else ()
-    p_main(*args)
+    if args or len(sys.argv) > 1:
+        p_main(*args)
+    else:
+        console()
+
+
+def console():
+    try:
+        import readline
+    except ImportError:
+        pass
+    compiler = CommandCompiler()
+    while True:
+        try:
+            code = input(">>> ")
+        except EOFError:
+            print()
+            break
+        while True:
+            try:
+                code = to_pure_python(code + "\n\n")
+                break
+            except SyntaxError as e:
+                if e.args[0] != UNCLOSED_BLOCK_ERROR:
+                    print_exc()
+                    code = ""
+                    break
+                try:
+                    code += "\n" + input("... ")
+                except EOFError:
+                    break
+        compiled = None
+        while True:
+            try:
+                compiled = compiler(code, "<console>", "single")
+            except SyntaxError:
+                try:
+                    compiled = compiler(code, "<console>", "exec")
+                except SyntaxError:
+                    print_exc()
+                    break
+            if compiled is not None:
+                break
+            try:
+                code += "\n" + input("... ")
+            except EOFError:
+                break
+        try:
+            exec(compiled or "")
+        except BaseException:
+            print_exc()
 
 
 def convert_main():
