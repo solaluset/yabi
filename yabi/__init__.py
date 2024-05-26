ca863b6cccc859e567a416eff630a830a5c04b55
solaluset <60041069+solaluset@users.noreply.github.com>
2024-05-26 09:44:13 +0300

    remake console

--- yabi/__init__.py
+++ yabi/__init__.py
@@ -1,12 +1,11 @@
 from __future__ import annotations
 import sys
 from io import TextIOBase
-from codeop import CommandCompiler
-from traceback import print_exc
+from code import InteractiveConsole
 
 import pwcp
 from pwcp import main as p_main
-from pwcp.preprocessor import PyPreprocessor, preprocess
+from pwcp.preprocessor import PyPreprocessor, PreprocessorError, preprocess
 
 from .parser import UNCLOSED_BLOCK_ERROR, to_bython, _to_pure_python_inner, to_pure_python
 
@@ -31,64 +30,44 @@ def main(args=None):
     if args or len(sys.argv) > 1:
         p_main(*args)
     else:
+        try:
+            import readline
+        except ImportError:
+            pass
         pwcp.hooks.install()
-        console()
+        YabiConsole().interact()
 
 
-def console():
-    try:
-        import readline
-    except ImportError:
-        pass
-    compiler = CommandCompiler()
-    preprocessor = PyPreprocessor(disabled=True)
-    namespace = {}
-    while True:
+class YabiConsole(InteractiveConsole):
+    def __init__(self):
+        super().__init__()
+        self.preprocessor = PyPreprocessor(disabled=True)
+        # compile is an attribute, not a function
+        self.compile, self._compiler = self._compiler, self.compile
+
+    def runsource(self, source, filename="<input>", symbol="single") -> bool:
         try:
-            code = input(">>> ")
-        except EOFError:
-            print()
-            break
-        code, parsed = _read_braced(code, preprocessor)
-        compiled = None
-        while True:
-            try:
-                compiled = compiler(parsed, "<console>", "single")
-            except SyntaxError:
-                try:
-                    compiled = compiler(parsed, "<console>", "exec")
-                except SyntaxError:
-                    print_exc()
-                    break
-            if compiled is not None:
-                break
-            try:
-                code += "\n" + input("... ")
-            except EOFError:
-                break
-            code, parsed = _read_braced(code, preprocessor)
-        try:
-            exec(compiled or "", namespace)
-        except BaseException:
-            print_exc()
-
-
-def _read_braced(code: str, preprocessor: PyPreprocessor) -> tuple[str, str]:
-    while True:
-        try:
-            parsed, first_block_is_braced = _to_pure_python_inner(preproc(code, preprocessor))
+            parsed, first_block_is_braced = _to_pure_python_inner(preproc(source, self.preprocessor))
+        except PreprocessorError:
+            self.showtraceback()
+            return False
         except SyntaxError as e:
-            if e.args[0] != UNCLOSED_BLOCK_ERROR:
-                print_exc()
-                return "", ""
-            try:
-                code += "\n" + input("... ")
-            except EOFError:
-                return "", ""
-        else:
-            if not first_block_is_braced and not code.endswith("\n"):
-                parsed = parsed.rstrip("\n")
-            return code, parsed
+            if e.args[0] == UNCLOSED_BLOCK_ERROR:
+                return True
+            self.showtraceback()
+            return False
+        if not first_block_is_braced and not source.endswith("\n"):
+            parsed = parsed.rstrip("\n")
+        return super().runsource(parsed, filename, symbol)
+
+    def _compiler(self, source, filename, symbol):
+        try:
+            return self._compiler(source, filename, symbol)
+        except PreprocessorError:
+            self.showtraceback()
+            return self._compiler("", filename, symbol)
+        except SyntaxError:
+            return self._compiler(source, filename, "exec")
 
 
 def convert_main():
