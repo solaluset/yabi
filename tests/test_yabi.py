a1317ce8b4ad937000710fc022364c60d657fcd6
solaluset <60041069+solaluset@users.noreply.github.com>
2024-05-29 11:28:41 +0300

    Revert "test"
    This reverts commit 2b1478b513f5f1832e3e0701a74bd0b63d99a26b.

--- tests/test_yabi.py
+++ tests/test_yabi.py
@@ -68,8 +68,6 @@ if True:
     print(2)
     print(3)
     """.strip() + "\n\n"
-    if sys.implementation.name == "pypy":
-        code = code.replace("(", "{", 1)
     with patch("sys.stdout", new=StringIO()), patch("sys.stdin", new=StringIO(code)):
         YabiConsole().interact()
         assert sys.stdout.getvalue() == """
